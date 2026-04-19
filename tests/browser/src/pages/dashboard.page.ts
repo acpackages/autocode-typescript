e2c1e5300caa8975c47e9aed99c8c74c9cdc76e2
Sanket Patel <shreeharitechnologies2018@gmail.com>
2026-04-19 17:02:07 +0530

    Reorganizing ac-runtime

--- tests/browser/src/pages/dashboard.page.ts
+++ tests/browser/src/pages/dashboard.page.ts
@@ -1,35 +1,9 @@
 import { AcElement } from "@autocode-ts/ac-runtime";
-import { SIDEBAR_NAV } from "../_app.export";
 
 @AcElement({
   selector: 'dashboard-page',
   template: `
-    <div class="app-page">
-      <app-header title="Dashboard"></app-header>
-      <div class="p-4 flex-fill overflow-auto">
-        <div class="row g-4">
-          <ac-container ac:for="let group of navItems">
-            <div class="col-12 col-md-6 col-lg-4" *if="group.children">
-              <div class="card h-100 shadow-sm border-0">
-                <div class="card-header bg-primary text-white py-3">
-                  <h5 class="card-title mb-0">{{group.label}}</h5>
-                </div>
-                <div class="card-body p-0">
-                  <div class="list-group list-group-flush">
-                    <ac-container ac:for="let child of group.children">
-                      <a [href]="child.route" class="list-group-item list-group-item-action py-3 px-4 d-flex align-items-center">
-                        <span class="flex-grow-1">{{child.label}}</span>
-                        <i class="fa-solid fa-chevron-right text-muted opacity-50 small"></i>
-                      </a>
-                    </ac-container>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </ac-container>
-        </div>
-      </div>
-    </div>
+    =
   `,
   styles: `
     .card {
@@ -44,5 +18,4 @@ import { SIDEBAR_NAV } from "../_app.export";
   `
 })
 export class DashboardPage {
-  navItems = SIDEBAR_NAV.filter(item => item.label !== 'Dashboard');
 }
